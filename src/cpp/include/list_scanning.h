e5ea147ee9d2603f5286527f46c1876125662d45
Jason <jasonmohoney@gmail.com>
2025-05-24 23:42:26 -0500

    verify numa

--- src/cpp/include/list_scanning.h
+++ src/cpp/include/list_scanning.h
@@ -538,26 +538,26 @@ inline void l2_blas(
             faiss::fvec_norms_L2sqr(norms_y, y + j0 * d, d, db_chunk);
 
             // use torch matmul
-            Tensor x_tensor = torch::from_blob((void*) x + i0 * d, {(int64_t) q_chunk, (int64_t) d}, torch::kFloat32);
-            Tensor y_tensor = torch::from_blob((void*) y + j0 * d, {(int64_t) db_chunk, (int64_t) d}, torch::kFloat32);
-            Tensor ip_tensor = torch::from_blob(ip_block, {(int64_t) q_chunk, (int64_t) db_chunk}, torch::kFloat32);
-            torch::matmul_out(ip_tensor, x_tensor, y_tensor.transpose(0, 1));
+            // Tensor x_tensor = torch::from_blob((void*) (x + i0 * d), {(int64_t) q_chunk, (int64_t) d}, torch::kFloat32);
+            // Tensor y_tensor = torch::from_blob((void*) (y + j0 * d), {(int64_t) db_chunk, (int64_t) d}, torch::kFloat32);
+            // Tensor ip_tensor = torch::from_blob(ip_block, {(int64_t) q_chunk, (int64_t) db_chunk}, torch::kFloat32);
+            // torch::matmul_out(ip_tensor, x_tensor, y_tensor.transpose(0, 1));
 
             /* SGEMM */
-            // {
-            //     const float one = 1.f;
-            //     float zero = 0.f;
-            //     FINTEGER nyi = FINTEGER(db_chunk);
-            //     FINTEGER nxi = FINTEGER(q_chunk);
-            //     FINTEGER di  = FINTEGER(d);
-            //     sgemm_("Transpose","Not transpose",
-            //            &nyi,&nxi,&di,
-            //            &one,
-            //            y + j0 * d, &di,
-            //            x + i0 * d, &di,
-            //            &zero,
-            //            ip_block,    &nyi);
-            // }
+            {
+                const float one = 1.f;
+                float zero = 0.f;
+                FINTEGER nyi = FINTEGER(db_chunk);
+                FINTEGER nxi = FINTEGER(q_chunk);
+                FINTEGER di  = FINTEGER(d);
+                sgemm_("Transpose","Not transpose",
+                       &nyi,&nxi,&di,
+                       &one,
+                       y + j0 * d, &di,
+                       x + i0 * d, &di,
+                       &zero,
+                       ip_block,    &nyi);
+            }
 
             // for (int64_t qi = 0; qi < (int64_t)q_chunk; ++qi) {
             //     float* line = ip_block + qi * db_chunk;
