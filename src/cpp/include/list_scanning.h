bdda82ac7891a6013618202ced4ccf7a6b0dbde7
Jason <jasonmohoney@gmail.com>
2025-05-24 23:34:43 -0500

    verify numa

--- src/cpp/include/list_scanning.h
+++ src/cpp/include/list_scanning.h
@@ -538,9 +538,9 @@ inline void l2_blas(
             faiss::fvec_norms_L2sqr(norms_y, y + j0 * d, d, db_chunk);
 
             // use torch matmul
-            Tensor x_tensor = torch::from_blob((void*) x + i0 * d, {q_chunk, d}, torch::kFloat32);
-            Tensor y_tensor = torch::from_blob((void*) y + j0 * d, {db_chunk, d}, torch::kFloat32);
-            Tensor ip_tensor = torch::from_blob(ip_block, {q_chunk, db_chunk}, torch::kFloat32);
+            Tensor x_tensor = torch::from_blob((void*) x + i0 * d, {(int64_t) q_chunk, (int64_t) d}, torch::kFloat32);
+            Tensor y_tensor = torch::from_blob((void*) y + j0 * d, {(int64_t) db_chunk, (int64_t) d}, torch::kFloat32);
+            Tensor ip_tensor = torch::from_blob(ip_block, {(int64_t) q_chunk, (int64_t) db_chunk}, torch::kFloat32);
             torch::matmul_out(ip_tensor, x_tensor, y_tensor.transpose(0, 1));
 
             /* SGEMM */
--- src/cpp/src/query_coordinator.cpp
+++ src/cpp/src/query_coordinator.cpp
@@ -408,20 +408,20 @@ void QueryCoordinator::handle_batched_job(const ScanJob &job,
     auto s5 = std::chrono::high_resolution_clock::now();
     res.enqueue_time_ns += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
-    // print out debug timing info s1, ... s5
-    std::cout << "QueryCoordinator::handle_batched_job: "
-              << "job_id: " << job.job_id
-                << ", core_id: " << res.core_id
-                << ", num_queries: " << job.num_queries
-                << ", partition_id: " << job.partition_id
-                << ", k: " << job.k
-                << ", rank: " << job.rank
-                << ", numa_node: " << node
-              << ", preamble: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s2 - s1).count()
-              << ", query copy: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s3 - s2).count()
-              << ", scan: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s4 - s3).count()
-              << ", enqueue: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - s4).count()
-              << std::endl;
+    // // print out debug timing info s1, ... s5
+    // std::cout << "QueryCoordinator::handle_batched_job: "
+    //           << "job_id: " << job.job_id
+    //             << ", core_id: " << res.core_id
+    //             << ", num_queries: " << job.num_queries
+    //             << ", partition_id: " << job.partition_id
+    //             << ", k: " << job.k
+    //             << ", rank: " << job.rank
+    //             << ", numa_node: " << node
+    //           << ", preamble: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s2 - s1).count()
+    //           << ", query copy: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s3 - s2).count()
+    //           << ", scan: " << std::chrono::duration_cast<std::chrono::nanoseconds>(s4 - s3).count()
+    //           << ", enqueue: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - s4).count()
+    //           << std::endl;
 }
 
 void QueryCoordinator::init_global_buffers(int64_t nQ,
