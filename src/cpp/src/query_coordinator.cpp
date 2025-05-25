44d63706adb22907fd85ef99baf750181c4a4a32
Jason <jasonmohoney@gmail.com>
2025-05-24 23:02:31 -0500

    verify numa

--- src/cpp/src/query_coordinator.cpp
+++ src/cpp/src/query_coordinator.cpp
@@ -347,37 +347,22 @@ void QueryCoordinator::handle_batched_job(const ScanJob &job,
     }
 
     // check that things are on the proper NUMA node
-    bool ok = true;
-    ok = ok && verify_numa_locality(qptr, "qptr");
-    ok = ok && verify_numa_locality(codes, "codes");
-    ok = ok && verify_numa_locality(ids, "ids");
-    ok = ok && verify_numa_locality(res.batch_queries, "batch_queries");
-    ok = ok && verify_numa_locality(res.blas_ip_block, "blas_ip_block");
-    ok = ok && verify_numa_locality(res.blas_norms_x, "blas_norms_x");
-    ok = ok && verify_numa_locality(res.blas_norms_y, "blas_norms_y");
-    for (int64_t i = 0; i < Q; ++i) {
-        ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->ord_, "ord_");
-        ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->vals_, "vals_");
-        ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->ids_, "ids_");
-    }
-    if (!ok) {
-        std::cerr << "[QueryCoordinator::handle_batched_job] NUMA locality check failed.\n";
-        // throw std::runtime_error("NUMA locality check failed");
-    }
-
-    // verify_numa_locality(qptr, "qptr");
-    // verify_numa_locality(codes, "codes");
-    // verify_numa_locality(ids, "ids");
-    // verify_numa_locality(res.batch_distances, "batch_distances");
-    // verify_numa_locality(res.batch_ids, "batch_ids");
-    // verify_numa_locality(res.blas_ip_block, "blas_ip_block");
-    // verify_numa_locality(res.blas_norms_x, "blas_norms_x");
-    // verify_numa_locality(res.blas_norms_y, "blas_norms_y");
-    //
+    // bool ok = true;
+    // ok = ok && verify_numa_locality(qptr, "qptr");
+    // ok = ok && verify_numa_locality(codes, "codes");
+    // ok = ok && verify_numa_locality(ids, "ids");
+    // ok = ok && verify_numa_locality(res.batch_queries, "batch_queries");
+    // ok = ok && verify_numa_locality(res.blas_ip_block, "blas_ip_block");
+    // ok = ok && verify_numa_locality(res.blas_norms_x, "blas_norms_x");
+    // ok = ok && verify_numa_locality(res.blas_norms_y, "blas_norms_y");
     // for (int64_t i = 0; i < Q; ++i) {
-    //     verify_numa_locality(res.topk_buffer_pool[i]->ord_, "ord_");
-    //     verify_numa_locality(res.topk_buffer_pool[i]->vals_, "vals_");
-    //     verify_numa_locality(res.topk_buffer_pool[i]->ids_, "ids_");
+    //     ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->ord_, "ord_");
+    //     ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->vals_, "vals_");
+    //     ok = ok && verify_numa_locality(res.topk_buffer_pool[i]->ids_, "ids_");
+    // }
+    // if (!ok) {
+    //     std::cerr << "[QueryCoordinator::handle_batched_job] NUMA locality check failed.\n";
+    //     // throw std::runtime_error("NUMA locality check failed");
     // }
 
     // run the scan on this chunk
